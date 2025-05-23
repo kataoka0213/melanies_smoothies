855f41841ed4ee4ead8f68076e148416c12973b2
kataoka0213 <m.kataoka@methodologic.co.jp>
2025-05-23 15:16:54 +0900

    Update streamlit_app.py
    move

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
-# Import python packages
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
-# Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """カスタムスムージーに使いたいフルーツを選択してください！
@@ -33,6 +32,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        # スムージールートの栄養情報を表示する新しいセクション
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -49,8 +51,4 @@ if ingredients_list:
 
         st.success('スムージーを注文しました、{}！'.format(name_on_order), icon="✅")
 
-# スムージールートの栄養情報を表示する新しいセクション
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
