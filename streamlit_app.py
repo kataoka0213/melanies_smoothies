249a661e373d54a301f565f56f7df960038c3c46
kataoka0213 <m.kataoka@methodologic.co.jp>
2025-05-23 15:48:32 +0900

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -16,7 +17,12 @@ st.write("スムージーの名前は次のようになります：", name_on_or
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# LOC関数を使用できるように、SnowparkデータフレームをPandasデータフレームに変換します。
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect (
